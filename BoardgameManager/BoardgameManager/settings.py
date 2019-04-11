8851876ba50e6dcaa83298d229f59632c8685047
Oh Chin Yang <iamohcy@gmail.com>
2019-04-12 02:20:10 +0800

    Modified settings for EB deployment

--- BoardgameManager/BoardgameManager/settings.py
+++ BoardgameManager/BoardgameManager/settings.py
@@ -91,16 +91,30 @@ WSGI_APPLICATION = 'BoardgameManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_bg',
-        'USER': 'iamohcy',
-        'PASSWORD': 'iamohcy2',
-        'HOST': 'localhost',
-        'PORT': '',
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'django_bg',
+            'USER': 'iamohcy',
+            'PASSWORD': 'iamohcy2',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
 
 
 # Password validation
